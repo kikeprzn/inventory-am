38fd7b08d7d0b8b070ff97fe5d5f9ac369e8346f
kikeprzn <61424495+eperezcfg@users.noreply.github.com>
2021-12-08 17:07:30 -0600

    convert serial number and provider in equipment to not required and to be able to add maintenance to past dates

--- src/ui/js/add_maintenance.js
+++ src/ui/js/add_maintenance.js
@@ -1,10 +1,10 @@
-const electron = require('electron'); 
-const { remote } = require('electron');
-const main = remote.require('./main');
+const electron = require("electron");
+const { remote } = require("electron");
+const main = remote.require("./main");
 
-const form = document.getElementById('maintenance_form');
-const provider = document.getElementById('provider');
-const equipment = document.getElementById('equipment');
+const form = document.getElementById("maintenance_form");
+const provider = document.getElementById("provider");
+const equipment = document.getElementById("equipment");
 
 const warning_alert = document.getElementById("warning_alert");
 
@@ -12,74 +12,70 @@ const success_alert = document.getElementById("success_alert");
 const success_text = document.getElementById("success_text");
 
 const maintenance_date = document.getElementById("maintenance_date");
-maintenance_date.setAttribute("min", actual_date);
+// maintenance_date.setAttribute("min", actual_date);
 
-
-function removeWarningAlert(){
-    warning_alert.classList.add("d-none");
+function removeWarningAlert() {
+  warning_alert.classList.add("d-none");
 }
-function showWarningAlert(){
-    warning_alert.classList.remove("d-none");
-}
-
-function removeSuccessAlert(){
-    success_text.innerHTML = "";
-    success_alert.classList.add("d-none");
-}
-function showSuccessAlert(text){
-    success_text.innerHTML = text;
-    success_alert.classList.remove("d-none");
+function showWarningAlert() {
+  warning_alert.classList.remove("d-none");
 }
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+function removeSuccessAlert() {
+  success_text.innerHTML = "";
+  success_alert.classList.add("d-none");
+}
+function showSuccessAlert(text) {
+  success_text.innerHTML = text;
+  success_alert.classList.remove("d-none");
+}
 
-    let maintenance_date = form.elements['maintenance_date']
-    let maintenance_time = form.elements['maintenance_time']
-    let type_of_service = form.elements['type_of_service']
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const new_maintenance = {
-        maintenance_date: maintenance_date.value,
-        maintenance_time: maintenance_time.value,
-        type_of_service: type_of_service.value,
-        equipment_id: parseInt(equipment.value), 
-        provider_id: (provider.value == "") ? null : parseInt (provider.value)
-    }
-    const result = await main.Maintenance.createMaintenance(new_maintenance);
-    showSuccessAlert("Elemento añadido")
-    form.reset();
-    provider.focus();
+  let maintenance_date = form.elements["maintenance_date"];
+  let maintenance_time = form.elements["maintenance_time"];
+  let type_of_service = form.elements["type_of_service"];
 
+  const new_maintenance = {
+    maintenance_date: maintenance_date.value,
+    maintenance_time: maintenance_time.value,
+    type_of_service: type_of_service.value,
+    equipment_id: parseInt(equipment.value),
+    provider_id: provider.value == "" ? null : parseInt(provider.value),
+  };
+  const result = await main.Maintenance.createMaintenance(new_maintenance);
+  showSuccessAlert("Elemento añadido");
+  form.reset();
+  provider.focus();
 });
 
-async function fillProviders(){
-    let result = await main.Provider.getAllProviders()
-    
-    result.forEach(item => {
-        provider.innerHTML += `
+async function fillProviders() {
+  let result = await main.Provider.getAllProviders();
+
+  result.forEach((item) => {
+    provider.innerHTML += `
             <option value = "${item.id_provider}">${item.name}</option>
         `;
-    });
-
+  });
 }
-async function fillEquipments(){
-    let result = await main.Equipment.getAllEquipments()
+async function fillEquipments() {
+  let result = await main.Equipment.getAllEquipments();
 
-    if (result.length == 0){
-        showWarningAlert()
-    }
+  if (result.length == 0) {
+    showWarningAlert();
+  }
 
-    result.forEach(item => {
-        equipment.innerHTML += `
+  result.forEach((item) => {
+    equipment.innerHTML += `
             <option value = "${item.id_equipment}">${item.name}</option>
         `;
-    });
-
+  });
 }
 
-async function init(){
-    fillProviders();
-    fillEquipments();
-} 
+async function init() {
+  fillProviders();
+  fillEquipments();
+}
 
-init()
+init();
